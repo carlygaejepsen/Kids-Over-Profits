816d992aca1af23b413d5a1da0bc6ac2b3be3ff1
carlygaejepsen <ttiresearch.dani@gmail.com>
2025-11-27 21:49:42 -0500

    rest points

--- functions.php
+++ functions.php
@@ -433,6 +433,24 @@ function kop_register_facilities_rest_routes() {
             'permission_callback' => '__return_true', // Publicly accessible
         )
     );
+
+    register_rest_route(
+        'kop/v1',
+        '/projects',
+        array(
+            'methods'  => WP_REST_Server::READABLE,
+            'callback' => function () {
+                $projects = kop_get_facilities_projects_from_database();
+
+                if (is_wp_error($projects)) {
+                    return $projects;
+                }
+
+                return rest_ensure_response($projects);
+            },
+            'permission_callback' => '__return_true', // Publicly accessible
+        )
+    );
 }
 add_action('rest_api_init', 'kop_register_facilities_rest_routes');
 
