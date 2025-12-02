698d565c33be7fc2e9a51e678d846c6e34d9f54c
carlygaejepsen <ttiresearch.dani@gmail.com>
2025-12-01 19:05:55 -0500

    Add page template wrappers for data forms

---
+++ page-admin-data.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Template Name: Admin Data Form
+ * Template Post Type: page
+ */
+
+include get_stylesheet_directory() . '/templates/data-form-admin.php';
---
+++ page-data.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Template Name: Public Data Form
+ * Template Post Type: page
+ */
+
+include get_stylesheet_directory() . '/templates/data-form-public.php';
