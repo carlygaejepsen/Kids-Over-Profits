36dcf65306e4f7c86d2137b5b34bef5835383cf5
carlygaejepsen <ttiresearch.dani@gmail.com>
2026-06-16 14:35:41 -0400

    cleanup & rest bug fix

--- tmp/check_addr.py
+++
@@ -1,9 +0,0 @@
-import json, glob
-
-for f in glob.glob('js/data/ccl*.json'):
-    for r in json.load(open(f, encoding='utf-8')):
-        for k in list(r.keys()):
-            v = r[k]
-            if isinstance(v, str) and 'Jeannette' in v:
-                fid = r.get('facility_id') or r.get('facility_number') or '?'
-                print(k + ' | fid=' + str(fid) + ' | ' + v[:300])
